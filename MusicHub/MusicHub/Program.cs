06c8c60b62ae703b8e7b58b43a05ff823ca9ed7f
izabellaT <izabelatodorova84@gmail.com>
2023-04-03 08:59:04 +0300

    test

---
+++ MusicHub/MusicHub/Program.cs
@@ -0,0 +1,15 @@
+﻿using MusicHub.Data;
+using System;
+
+namespace MusicHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new MusicHubContext();
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+        }
+    }
+}
