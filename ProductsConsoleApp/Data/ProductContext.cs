39991b0cff02ae1036feaa7843708c1756f3a414
izabellaT <izabelatodorova84@gmail.com>
2023-04-20 10:49:14 +0300

    vgguyguy

---
+++ ProductsConsoleApp/Data/ProductContext.cs
@@ -0,0 +1,19 @@
+﻿using Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Data
+{
+    public class ProductContext:DbContext
+    {
+        public ProductContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.; Integrated Security=true; Database=ProductDb");
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ ProductsConsoleApp/ProductsConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using ProductsConsoleApp.Presentation;
+using System;
+
+namespace ProductsConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Display display = new Display();
+        }
+    }
+}
