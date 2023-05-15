ad04776aa36d91a81c1dfda3fd92e35bac9c827b
izabellaT <izabelatodorova84@gmail.com>
2023-05-15 09:31:24 +0300

    yfregfr

---
+++ DogsAppWeb/DogsAppWeb/Controllers/DogsController.cs
@@ -0,0 +1,70 @@
+﻿using DogsAppWeb.Data;
+using DogsAppWeb.Domain;
+using DogsAppWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogsAppWeb.Controllers
+{
+    public class DogsController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public DogsController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(DogCreateViewModel bindingModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Dog dogFromDb = new Dog
+                {
+                    Name = bindingModel.Name,
+                    Age = bindingModel.Age,
+                    Breed = bindingModel.Breed,
+                    Picture = bindingModel.Picture
+                };
+
+                context.Dogs.Add(dogFromDb);
+                context.SaveChanges();
+
+                return this.RedirectToAction("Success");
+            }
+            return this.View();
+        }
+
+        public IActionResult Success()
+        {
+            return this.View();
+        }
+        public IActionResult All()
+        {
+            List<DogAllViewModel> dogs = context.Dogs.Select(dogFromDb => new DogAllViewModel
+            {
+                Id = dogFromDb.Id,
+                Name = dogFromDb.Name,
+                Age = dogFromDb.Age,
+                Breed = dogFromDb.Breed,
+                Picture = dogFromDb.Picture
+            }
+            ).ToList();
+            return View(dogs);
+        }
+
+    }
+}
---
+++ DogsAppWeb/DogsAppWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DogsAppWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
