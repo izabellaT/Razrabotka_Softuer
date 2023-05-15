e84a2665ccb5d4ef89204affa204b4b994dab8be
izabellaT <izabelatodorova84@gmail.com>
2023-05-15 08:40:59 +0300

    hjvrfhf

---
+++ AirLineTicketsApp/AirLineTicketsApp/Entities/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirLineTicketsApp.Entities
+{
+    public class Client
+    {
+        public Client()
+        {
+            this.Reservations = new HashSet<Reservation>();
+        }
+        public int Id { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+        public string Address { get; set; }
+        public ICollection<Reservation> Reservations { get; set; }
+    }
+}
---
+++ AirLineTicketsApp/AirLineTicketsApp/Entities/Plane.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirLineTicketsApp.Entities
+{
+    public class Plane
+    {
+        public Plane()
+        {
+            this.Flights = new HashSet<Flight>();
+        }
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Model { get; set; }
+        public string Image { get; set; }
+        public double Obem { get; set; }
+        public int Bar { get; set; }
+        public int Mesta { get; set; }
+
+        public ICollection<Flight> Flights { get; set; }
+    }
+}
